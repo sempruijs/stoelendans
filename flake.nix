1f2c477432384e622dc994a98c3df9e68b5957ad
sem pruijs <sem@pruijs.net>
2024-09-03 15:59:20 +0200

    Init flake

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Website for intresting questions.";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = { self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit self; } {
+      imports = [];
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells = {
+          default = pkgs.mkShell {
+              buildInputs = with pkgs; [ nodePackages.typescript nodePackages.typescript-language-server];
+          };
+        };
+        packages = rec {
+          default = site;
+
+          site = pkgs.stdenv.mkDerivation {
+            buildInputs = with pkgs; [ nodePackages.typescript ];
+            src = ./site;
+            name = "site";
+            buildPhase = ''
+              # cd ts
+              # tsc --build
+              # cd ..
+            '';
+
+            installPhase = ''
+              mkdir $out
+              # mkdir $out/ts
+              cp ./index.html $out
+              # cp -r ./css $out/css
+              # cp -r ./content $out
+              # cp ./ts/index.js $out/ts
+            '';
+          };
+        };
+      };
+      flake = {};
+    };
+}
