46c5a980242d117f34aae127143866142534ab88
sem pruijs <sem@pruijs.net>
2024-09-03 20:55:41 +0200

    Let the button generate a new question

--- site/ts/99_index.ts
+++ site/ts/99_index.ts
@@ -1,9 +1,13 @@
-function random_element<T>(xs: T[]): T {
-    return xs[Math.floor(Math.random() * xs.length)];
-}
-
-function hello_world() {
+function next_question() {
     // questions comes from the questions.ts file
     let random_question = random_element(questions);
-    console.log(random_question);
+    show_question(random_question);
+}
+
+function show_question(s: string) {
+    document.getElementById("question").textContent=s;
+}
+
+function random_element<T>(xs: T[]): T {
+    return xs[Math.floor(Math.random() * xs.length)];
 }
