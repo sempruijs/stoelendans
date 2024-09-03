ab27653e1d2f22886fafacb930c8e53d7040d7d0
sem pruijs <sem@pruijs.net>
2024-09-03 20:48:32 +0200

    Make random_element function

--- site/ts/99_index.ts
+++ site/ts/99_index.ts
@@ -1,4 +1,9 @@
+function random_element<T>(xs: T[]): T {
+    return xs[Math.floor(Math.random() * xs.length)];
+}
+
 function hello_world() {
     // questions comes from the questions.ts file
-    console.log(questions);
+    let random_question = random_element(questions);
+    console.log(random_question);
 }
