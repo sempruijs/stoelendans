ca7c82a2fdd33c44392c5c8285ab6efae9a5cff6
sem pruijs <sem@pruijs.net>
2024-09-05 18:47:19 +0200

    Add example question data

---
+++ site/ts/03_multiple_choice_questions.ts
@@ -0,0 +1,10 @@
+let multiple_choice_questions: MultipleChoice[] = [
+    {
+        question: "Wie was de president van Amerika in 2016?",
+        right_answer: "Donald Trump",
+        wrong_answers: [
+            "Joe Biden",
+            "Mijn mama"
+        ]
+    }
+]
