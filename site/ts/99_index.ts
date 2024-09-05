b4d07c7333968f3b102d87ed1d28e195b19d2887
sem pruijs <sem@pruijs.net>
2024-09-05 20:38:25 +0200

    Show correctness

--- site/ts/99_index.ts
+++ site/ts/99_index.ts
@@ -16,20 +16,34 @@ function set_button_container(html_code: string) {
     document.getElementById("button_container").innerHTML = html_code;
 }
 
+function show_correctness(result: boolean) {
+    var answer = "";
+    if (result) {
+        answer = "Goed!";
+    } else {
+        answer = "Fout";
+    }
+    document.getElementById("correctness").textContent = answer;
+}
+
+function reset_correctness() {
+    document.getElementById("correctness").textContent = "";
+}
+
 function next_mc_question() {
     let random_question: MultipleChoice = random_element(mc_questions);
     show_mc_question(random_question);
     let new_buttons = generate_buttons(random_question);
     console.log(new_buttons);
     set_button_container(new_buttons);
+    reset_correctness();
 }
 
 function generate_button(answer: string, is_right: boolean): string {
     if (is_right) {
-        console.log("blz");
-        return "<button onclick=\"console.log(\"right\")\">" + answer + "</button>";
+        return "<button onclick=\"show_correctness(true)\">" + answer + "</button>";
     }
-    return "<button onclick=\"console.log(\"wrong\")\">" + answer + "</button>";
+    return "<button onclick=\"show_correctness(false)\")\">" + answer + "</button>";
 }
 
 function generate_buttons(q: MultipleChoice): string {
