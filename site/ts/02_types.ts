6e8947a8566227ea8943d03677d58db9b5d6be1e
sem pruijs <sem@pruijs.net>
2024-09-05 18:47:19 +0200

    Add example question data

--- site/ts/02_types.ts
+++ site/ts/02_types.ts
@@ -1,14 +1,6 @@
-interface Question {
-    question: string;
-}
-
-interface Answer {
-    answer: string;
-}
-
 interface MultipleChoice {
-    question: Question;
-    right_answer: Answer;
-    wrong_answers: Answer[];
+    question: String;
+    right_answer: String;
+    wrong_answers: String[];
 }
 
