b812a77bebc199cbe83c80455205ddffef39d8ed
Jisse11 <jisse@jissebrus.com>
2024-09-12 11:38:02 +0200

    space next question

--- site/ts/07_keyboardhandler.ts
+++ site/ts/07_keyboardhandler.ts
@@ -3,12 +3,22 @@ function handlekeyboardinput(event: KeyboardEvent): void     {
     
     if (event.key === ' ') {
         next_question()  ;
-        next_mc_question()
       }
 
 
 
 }
+function handlekeyboardinputmc(event: KeyboardEvent): void     {
+  const key = event.key;
+  
+  if (event.key === ' ') {
+    next_mc_question()
+    ;
+    }
 
+
+
+}
 
 window.addEventListener('keydown', handlekeyboardinput);
+window.addEventListener('keydown', handlekeyboardinputmc);
